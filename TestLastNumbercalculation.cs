02173b8422d6adc1e8f4d91845199df431a84850
AndreasCordeus <andreas_cordeus@hotmail.com>
2024-01-19 14:29:34 +0100

    added docker image

--- TestLastNumbercalculation.cs
+++ TestLastNumbercalculation.cs
@@ -12,7 +12,7 @@ public class TestLastNumberCalculation {
     }
         [Fact]
     public void TestValidateSocialSecurityNumber_ReturnFalse() {
-        string personNr = "9502141635";
+        string personNr = "9001152396";
 
         bool result = LastNumberCalculation.ValidateSocialSecurityNumber(personNr);
 
